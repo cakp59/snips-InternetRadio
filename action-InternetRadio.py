26ec40547d0cd722c81c6659e54efb9ffa0364e5
cakp59 <50278507+cakp59@users.noreply.github.com>
2019-05-06 18:46:09 +0200

    Update action-InternetRadio.py

--- action-InternetRadio.py
+++ action-InternetRadio.py
@@ -14,10 +14,8 @@ class SnipsConfigParser(configparser.SafeConfigParser):
     def to_dict(self):
         return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
 
-
 def read_configuration_file(configuration_file):
-  print("Entrée - cakp59:InternetRadio - read_configuration_file")
-
+    print("Entrée - cakp59:InternetRadio - read_configuration_file")
     try:
         with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
             conf_parser = SnipsConfigParser()
@@ -31,38 +29,34 @@ def subscribe_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
     action_wrapper(hermes, intentMessage, conf)
 
-
 def action_wrapper(hermes, intentMessage, conf):
-    """ Write the body of the function that will be executed once the intent is recognized. 
-    In your scope, you have the following objects : 
+    """ Write the body of the function that will be executed once the intent is recognized.
+    In your scope, you have the following objects :
     - intentMessage : an object that represents the recognized intent
     - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
-    - conf : a dictionary that holds the skills parameters you defined. 
-      To access global parameters use conf['global']['parameterName']. For end-user parameters use conf['secret']['parameterName'] 
-     
-    Refer to the documentation for further details. 
-    """ 
-    print("Entrée - cakp59:InternetRadio - action_wrapper")   
+    - conf : a dictionary that holds the skills parameters you defined.
+      To access global parameters use conf['global']['parameterName']. For end-user parameters use conf['secret']['parameterName']
 
+    Refer to the documentation for further details.
+    """
+    print("Entrée - cakp59:InternetRadio - action_wrapper")
     import subprocess
-    
     try:
         command=intentMessage.slots.selectRadioStation.first().value
-        subprocess.call( "mpc "+command, shell=True)
-        hermes.publish_end_session(intentMessage.session_id,"")  
+        subprocess.call( "mpc clear ", shell=True)
+        subprocess.call( "mpc load snips.playlist.radio.txt ", shell=True)
+        subprocess.call( "mpc  "+command, shell=True)
+        hermes.publish_end_session(intentMessage.session_id,"")
     except:
         print("Error with command - cakp59:InternetRadio - selectRadioStation")
-        #hermes.publish_end_session(intentMessage.session_id,"Error - selectRadioStation")
+        hermes.publish_end_session(intentMessage.session_id,"Error - selectRadioStation")
 #    try:
-#        playlist=intentMessage.slots.load_list.first().value
-#        subprocess.call( "mpc clear", shell=True)
-#        subprocess.call( "mpc "+playlist, shell=True)
-#        subprocess.call( "mpc play", shell=True)
-#        hermes.publish_end_session(intentMessage.session_id,"")  
+#        command=intentMessage.slots.setRadioStationVolume.first().value
+#        subprocess.call( "mpc  "+command, shell=True)
+#        hermes.publish_end_session(intentMessage.session_id,"")
 #    except:
 #        print("Error with command - cakp59:InternetRadio - setRadioStationVolume")
-        #hermes.publish_end_session(intentMessage.session_id,"Error - setRadioStationVolume")
-
+#        hermes.publish_end_session(intentMessage.session_id,"Error - setRadioStationVolume")
 
 if __name__ == "__main__":
     print("Entrée - cakp59:InternetRadio - main")
