661766d6396203864d055667cf1aa2accd72fbdf
cakp59 <50278507+cakp59@users.noreply.github.com>
2019-05-10 21:08:54 +0200

    Update action-selectInternetRadioStation.py

--- action-selectInternetRadioStation.py
+++ action-selectInternetRadioStation.py
@@ -44,7 +44,7 @@ def action_wrapper(hermes, intentMessage, conf):
         command=intentMessage.slots.SelectedStation.first().value
         command= "mpc "+command
         subprocess.call(command, shell=True)
-#        ErrMess="snips-InternetRadio - command OK - SelectedStation - command="+command
+#        ErrMess="snips-InternetRadio - command OK - SelectedStation - command= "+command
         hermes.publish_end_session(intentMessage.session_id,ErrMess)
     except:
         ErrMess="snips-InternetRadio - command KO - SelectedStation - command="+command
