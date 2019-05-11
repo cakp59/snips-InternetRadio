f2924c4d45c85a34c24f7ab63b9cb24fe2427ae4
cakp59 <50278507+cakp59@users.noreply.github.com>
2019-05-11 08:35:16 +0200

    Update action-setInternetRadioStationVolume.py

--- action-setInternetRadioStationVolume.py
+++ action-setInternetRadioStationVolume.py
@@ -46,7 +46,7 @@ def action_wrapper(hermes, intentMessage, conf):
             command= "amixer -c1 set Playback,0  "+command[5:7]+"%"
         subprocess.call( command, shell=True)
         ErrMess=""
-        ErrMess="snips-InternetRadio - command OK - MyRadioStationVolume - command= "+command
+#        ErrMess="snips-InternetRadio - command OK - MyRadioStationVolume - command= "+command
         hermes.publish_end_session(intentMessage.session_id,ErrMess)
     except:
         ErrMess="snips-InternetRadio - command KO - MyRadioStationVolume - command= "+command
